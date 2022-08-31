f35a93a121cf95ebcb4a3a538204963a2a36f7b9
Pavel Gaidukov <mailgpn@gmail.com>
2022-08-31 20:10:32 +0500

    edit Dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,3 +6,6 @@ python manage.py migrate
 python manage.py createcachetable
 python manage.py collectstatic  --noinput
 python manage.py loaddata db.json
+gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
+
+exec "$@"
